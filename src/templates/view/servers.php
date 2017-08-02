f79df56a56622c2fb83cd74aa9803f02648c7961
Hypolite Petovan <mrpetovan@gmail.com>
2017-08-01 21:34:09 -0400

    Change Friendica self-hosting URL

--- src/templates/view/servers.php
+++ src/templates/view/servers.php
@@ -2,7 +2,7 @@
     <div class="sites">
         <h1>Public servers</h1>
         <p class="intro">
-            If you are not interested in <a href="http://friendica.com/download">hosting your own server</a>, you can still use Friendica.
+            If you are not interested in <a href="http://friendi.ca/installation/">hosting your own server</a>, you can still use Friendica.
             Here are some public server run by enthousiasts that you can join.
             We recommend these based on their <abbr title="Decent speed, proper security, recent version, etc.">health</abbr>.
         </p>
